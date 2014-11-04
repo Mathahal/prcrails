7cc918dc3cdf7200782b0f3e977568c753bcbbed
Mashliz <miavivale@gmail.com>
2014-11-05 01:55:15 +0900

    edited

--- pr/app/controllers/view_controller.rb
+++ pr/app/controllers/view_controller.rb
@@ -1,6 +1,11 @@
 class ViewController < ApplicationController
   def select
-    @review = Review.new
-    @authors = Author.all
+    # @review = Review.new
+    # @authors = Author.all
+
+    # @book = Book.new(publish: 'ソシム')
+    # @books = Book.all
+
+    @book = Book.find(1)
   end
 end
